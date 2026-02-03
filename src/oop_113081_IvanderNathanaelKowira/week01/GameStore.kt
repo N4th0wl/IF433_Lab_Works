f277e7c75d4bd2cb7dff4def03a07e6b38cd07ab
LArchitettos <ivandernathanaelk@gmail.com>
2026-02-03 20:14:28 +0700

    week01 : (tugas 2) implement receipt printing

--- src/oop_113081_IvanderNathanaelKowira/week01/GameStore.kt
+++ src/oop_113081_IvanderNathanaelKowira/week01/GameStore.kt
@@ -3,6 +3,18 @@ package oop_113081_IvanderNathanaelKowira.week01
 fun main() {
     val gameTitle: String = "OOP LAB MENYENANGKAN"
     val price: Int = 650000 // Harga 1 SKS
+
+    val finalPrice: Int = calculatedDiscount(price)
+
+    printReceipt(title = gameTitle, finalPrice = finalPrice)
 }
+
 fun calculatedDiscount(price: Int) = if (price > 500000) price * 80 / 100 else price * 90 / 100
 
+fun printReceipt(title: String, finalPrice: Int) {
+    println("======= STEAMWOKWOK =======")
+    println("--------- by Ivan ---------")
+    println("Judul Game: $title")
+    println("Harga Akhir: Rp. $finalPrice")
+    println("===========================")
+}
