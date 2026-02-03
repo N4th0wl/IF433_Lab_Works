9f9ba40656b4dbf551b70025d29b4edf5d98e093
LArchitettos <ivandernathanaelk@gmail.com>
2026-02-03 20:04:19 +0700

    week01 : (tugas 2) implement discount logic

--- src/oop_113081_IvanderNathanaelKowira/week01/GameStore.kt
+++ src/oop_113081_IvanderNathanaelKowira/week01/GameStore.kt
@@ -1,2 +1,8 @@
 package oop_113081_IvanderNathanaelKowira.week01
 
+fun main() {
+    val gameTitle: String = "OOP LAB MENYENANGKAN"
+    val price: Int = 650000 // Harga 1 SKS
+}
+fun calculatedDiscount(price: Int) = if (price > 500000) price * 80 / 100 else price * 90 / 100
+
