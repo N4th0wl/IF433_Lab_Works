16e9124da129c126bcfc909867985a274bc63232
LArchitettos <ivandernathanaelk@gmail.com>
2026-02-10 20:15:37 +0700

    week02: challenge refactor primary constructor using default args

--- src/oop_113081_IvanderNathanaelKowira/week02/Student.kt
+++ src/oop_113081_IvanderNathanaelKowira/week02/Student.kt
@@ -1,6 +1,11 @@
 package oop_113081_IvanderNathanaelKowira.week02
 
-class Student (val name: String, val nim: String, var major: String) {
+class Student (
+    val name: String,
+    val nim: String,
+    var major: String,
+    var gpa: Double = 0.0 // <- Default Argument
+) {
     init {
         // Validasi sederhana: Cek panjang NIM
         if (nim.length != 5) {
