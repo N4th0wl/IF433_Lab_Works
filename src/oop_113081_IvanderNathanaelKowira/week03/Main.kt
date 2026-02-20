b61b052dfbc08a7dcacd55a59e0f16fd416d5db3
LArchitettos <ivandernathanaelk@gmail.com>
2026-02-20 15:09:26 +0700

    week03: (task 1) implement Weapon testing in main

--- src/oop_113081_IvanderNathanaelKowira/week03/Main.kt
+++ src/oop_113081_IvanderNathanaelKowira/week03/Main.kt
@@ -1,10 +1,18 @@
 package oop_113081_IvanderNathanaelKowira.week03
 
 fun main() {
+    // Dari guided practicum
+    /*
     val e = Employee("Budi")
     e.salary = -1000
     e.salary = 5000000
     println("Gaji: ${e.salary}")
     e.increasePerformance()
     println("Pajak yang harus dibayar: ${e.tax}")
+    */
+    val weapon = Weapon("Pedangnya Pak Wirawan")
+    // weapon.damage = -50
+    weapon.damage = 9999
+    println("Damage sekarang: ${weapon.damage}")
+    println("Tier senjata: ${weapon.tier}")
 }
