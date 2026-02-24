ab17b9d2ada292f3b20c23a9e123e8788cb7803e
LArchitettos <ivandernathanaelk@gmail.com>
2026-02-24 14:46:35 +0700

    week04: create Car class extending Vehicle

---
+++ src/oop_113081_IvanderNathanaelKowira/week04/Car.kt
@@ -0,0 +1,10 @@
+package oop_113081_IvanderNathanaelKowira.week04
+
+// Car "Is-A" Vehicle, Parameter brand dilempar ke atas melalui Vehicle(brand)
+open class Car(brand: String, val numberOfDoors: Int) : Vehicle(brand) {
+
+    fun openTrunk() {
+        println("Bagasi mobil $brand dengan $numberOfDoors pintu dibuka")
+    }
+
+}
