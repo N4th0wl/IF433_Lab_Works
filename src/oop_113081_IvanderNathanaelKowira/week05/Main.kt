9ac6f430842faeeab2614a8134c0507fb02623dc
N4th0wl <ivandernathanaelk@gmail.com>
2026-03-04 15:34:56 +0700

    week05: implement polymorphic collection and smart casting

---
+++ src/oop_113081_IvanderNathanaelKowira/week05/Main.kt
@@ -0,0 +1,31 @@
+package oop_113081_IvanderNathanaelKowira.week05
+
+fun main() {
+    val dosen1 = Dosen("Pak Alex", "0123456")
+    val admin1 = Admin("Bu Siti")
+
+    // Polymorphic Collection: List yang berisi tipe Parent, tapi isinya objek Anak
+    val daftarPegawai: List<Pegawai> = listOf(dosen1, admin1)
+
+    println("=== AKTIVITAS PEGAWAI ===")
+    for(pegawai in daftarPegawai) {
+        // Pemanggilan Runtime Polymorphism
+        pegawai.bekerja()
+
+        // pegawai.mengajar() // INI AKAN ERROR karena tipe referensinya adalah pegawai
+
+        // Smart Casting dengan is dan when
+        when(pegawai) {
+            is Dosen -> {
+                println("=> Terdeteksi sebagai Dosen (NIDN: ${pegawai.nidn})")
+                pegawai.mengajar()
+            }
+            is Admin -> {
+                println("=> Terdeteksi sebagai Admin")
+                pegawai.doAdminWork()
+            }
+        }
+        println("------------------------------------")
+    }
+
+}
