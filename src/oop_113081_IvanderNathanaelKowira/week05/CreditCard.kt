5a159c78aec8ff7db0b2784837c3b09d1d8216f3
N4th0wl <ivandernathanaelk@gmail.com>
2026-03-04 16:15:11 +0700

    week05: (task 2) create CreditCard class

---
+++ src/oop_113081_IvanderNathanaelKowira/week05/CreditCard.kt
@@ -0,0 +1,15 @@
+package oop_113081_IvanderNathanaelKowira.week05
+
+class CreditCard(accountName: String, val limit: Double) : PaymentMethod(accountName) {
+    var usedAmount: Double = 0.0
+
+    override fun processPayment(amount: Double) {
+        if (usedAmount + amount <= limit) {
+            usedAmount += amount
+            println("Pembayaran berhasil menggunakan Credit Card milik $accountName")
+            println("Total penggunaan kartu: $usedAmount")
+        } else {
+            println("Transaksi ditolak. Melebihi limit kartu.")
+        }
+    }
+}
