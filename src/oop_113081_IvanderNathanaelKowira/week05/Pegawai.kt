7a561961dd68b1658c7f9b58e9267ff250e6615f
N4th0wl <ivandernathanaelk@gmail.com>
2026-03-04 14:23:01 +0700

    week05: create abstract class Pegawai

---
+++ src/oop_113081_IvanderNathanaelKowira/week05/Pegawai.kt
@@ -0,0 +1,6 @@
+package oop_113081_IvanderNathanaelKowira.week05
+
+abstract class Pegawai(val nama: String) {
+    // Abstract Function tidak memiliki body (tanpa kurung kurawal)
+    abstract fun bekerja()
+}
