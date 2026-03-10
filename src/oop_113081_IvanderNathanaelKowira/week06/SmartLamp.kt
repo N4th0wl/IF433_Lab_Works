504ec908fd4281529e4f59c3b32375f4714a19e9
LArchitettos <ivandernathanaelk@gmail.com>
2026-03-10 15:32:55 +0700

    week06: (task) create SmartLamp

---
+++ src/oop_113081_IvanderNathanaelKowira/week06/SmartLamp.kt
@@ -0,0 +1,10 @@
+package oop_113081_IvanderNathanaelKowira.week06
+
+class SmartLamp(override val id: String, override val name: String) : SmartDevice, Switchable {
+    override fun turnOn() {
+        println("Lampu $name dinyalakan.")
+    }
+    override fun turnOff() {
+        println("Lampu $name dimatikan.")
+    }
+}
