58746b28804182c8219b137e9d11997407772ca9
LArchitettos <ivandernathanaelk@gmail.com>
2026-03-10 15:07:59 +0700

    week06: create compound interfaces

---
+++ src/oop_113081_IvanderNathanaelKowira/week06/Capabilities.kt
@@ -0,0 +1,10 @@
+package oop_113081_IvanderNathanaelKowira.week06
+
+// Interface = Can-Do (Behavior)
+interface BluetoothConnectable {
+    fun connectToBluetooth()
+}
+
+interface Rechargaable {
+    fun chargeBattery()
+}
