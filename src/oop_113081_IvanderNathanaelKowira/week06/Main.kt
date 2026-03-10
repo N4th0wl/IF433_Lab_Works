9ebce36f82a56cea4df39b44f2f92d9e900ab7ab
LArchitettos <ivandernathanaelk@gmail.com>
2026-03-10 15:45:41 +0700

    week06: (task) instantiate smart devices in main

--- src/oop_113081_IvanderNathanaelKowira/week06/Main.kt
+++ src/oop_113081_IvanderNathanaelKowira/week06/Main.kt
@@ -18,4 +18,10 @@ fun main() {
     println("\n=== TESTING CHECKOUT ===")
     processCheckout(pay1, 50000.0)
     processCheckout(pay2, 150000.0)
+
+    println("\n=== TESTING SMARTDEVICES ===")
+    // Inisiasi Device
+    val lamp = SmartLamp("L1", "Ruang Tamu")
+    val speaker = SmartSpeaker("S1", "Google Nest Dapur")
+    val cctv = SmartCCTV("C1", "Ezviz Garasi")
 }
