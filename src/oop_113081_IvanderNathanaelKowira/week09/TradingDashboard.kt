5fbb14abca545bdc56127cde6229abeda946f238
N4th0wl <ivandernathanaelk@gmail.com>
2026-04-21 18:31:45 +0700

    week09: (task) print worst performers and unique pairs

--- src/oop_113081_IvanderNathanaelKowira/week09/TradingDashboard.kt
+++ src/oop_113081_IvanderNathanaelKowira/week09/TradingDashboard.kt
@@ -46,5 +46,10 @@ fun main() {
     println("\n--- TOP PERFORMERS (WIN) ---")
     topPerformersString.forEach { println(it) }
 
+    println("\n--- WORST PERFORMERS (LOSS) ---")
+    worstPerformersString.forEach { println(it) }
+    println("\n--- UNIQUE TRADING PAIRS ---")
+    println(uniquePairs)
+
 
 }
