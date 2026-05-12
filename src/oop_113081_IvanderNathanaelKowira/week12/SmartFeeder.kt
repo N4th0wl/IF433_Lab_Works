403c559d5c24d3972f7921e7ef9aa67dcc6a7c5d
N4th0wl <ivandernathanaelk@gmail.com>
2026-05-12 09:36:21 +0700

    week12: (task) handle feeding execution with multiple catch

--- src/oop_113081_IvanderNathanaelKowira/week12/SmartFeeder.kt
+++ src/oop_113081_IvanderNathanaelKowira/week12/SmartFeeder.kt
@@ -18,4 +18,15 @@ fun dispenseKibble(requestedGram: Int, availableGram: Int, isJammed: Boolean): I
 
 fun main() {
     var currentKibbleStock = 50
+
+    println("==== JADWAL MAKAN 1 (PAGI) ====")
+    try {
+        currentKibbleStock = dispenseKibble(requestedGram = 80, availableGram = currentKibbleStock, isJammed = false)
+    } catch (e: DispenserJamException) {
+        println("Caught Hardware Error: ${e.message}")
+    } catch (e: FoodEmptyException) {
+        println("Caught Domain Error: ${e.message}")
+    } catch (e: Exception) {
+        println("Caught General Error: Terjadi kesalahan tidak terduga, ${e.message}")
+    }
 }
