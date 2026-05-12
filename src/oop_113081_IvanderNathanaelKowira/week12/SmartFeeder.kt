90916f4d7b04fa2384e6194f7294e112489ccdb0
N4th0wl <ivandernathanaelk@gmail.com>
2026-05-12 09:38:31 +0700

    week12: (task) execute feeding using runCatching

--- src/oop_113081_IvanderNathanaelKowira/week12/SmartFeeder.kt
+++ src/oop_113081_IvanderNathanaelKowira/week12/SmartFeeder.kt
@@ -31,4 +31,9 @@ fun main() {
     } finally {
         println("Siklus pengecekan dispenser pagi selesai.")
     }
+
+    println("\n==== JADWAL MAKAN SORE ====")
+    runCatching {
+        dispenseKibble(requestedGram = 30, availableGram = 1000, isJammed = false)
+    }
 }
