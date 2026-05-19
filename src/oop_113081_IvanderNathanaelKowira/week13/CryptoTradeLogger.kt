11f97605e3d2f2d26f09dbee592323a9ebd08f2b
N4th0wl <ivandernathanaelk@gmail.com>
2026-05-19 19:07:44 +0700

    week13: (task) create TradeRecord data class

---
+++ src/oop_113081_IvanderNathanaelKowira/week13/CryptoTradeLogger.kt
@@ -0,0 +1,10 @@
+package oop_113081_IvanderNathanaelKowira.week13
+
+// Data class untuk menampung record transaksi kripto
+data class TradeRecord(
+    val id: Int,
+    val symbol: String,
+    val type: String,
+    val margin: Double,
+    val pnl: Double
+)
